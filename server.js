02ec2f29f877d9abd4e1df0d352bc35e165bafc2
Brian Richardson <97001156+b-richardson@users.noreply.github.com>
2023-03-01 13:11:28 -0500

    Both front and backend up and running

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const port = 3000;
+const port = 5000;
 const connectDB = require("./config/database");
 
 const app = express();
@@ -11,11 +11,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
 
 app.listen(port, () => {
-    console.log("Server is running, you better catch it!");
+    console.log(`Server is running on port ${port}, you better catch it!`);
   });
   
